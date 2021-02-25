6d13a9ac6137b7da8eb542e9436eea1404423f50
svalencia <svalencia@gmail.com>
2021-02-24 22:11:14 -0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
 
- const App = () =>(
+import "./App.css";
+
+const App = () => {
+  const [respuesta, setRespuesta] = useState({});
+
+  useEffect(() => {
+    saludo();
+  }, []);
+
+  const saludo = async () => {
+    let url = "http://127.0.0.1:8000/testing/";
+    let response = await fetch(url);
+    let res = await response.json();
+    console.log(res)
+    setRespuesta(res);
+  };
+  
+
+  return (
     <div className="App">
-      prueva tecnica
+      {respuesta.message}
+     
     </div>
   );
-
+};
 export default App;
