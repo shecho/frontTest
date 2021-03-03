844c97331b7a89086d188884a46c871dbe9d3125
shecho <shechovala@gmail.com>
2021-03-02 22:22:02 -0500

    car inputs

--- src/components/cars/car.jsx
+++ src/components/cars/car.jsx
@@ -1,23 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Car = (props) => {
-    const { marca, fabrication_time } = props.car
-    return (
-        <div className="p-3 col-md-3">
-            <div className="card justify-content-center p-1">
-                <div className="d-flex justify-content-around">
-                    <i className="fa fa-heart mx-2 btn btn-outline-danger" aria-hidden="true"></i>
-                    <i className="fa fa-pencil mx-2 btn btn-warning" aria-hidden="true"></i>
-                    <i className="fa fa-trash mx-2 btn btn-danger" aria-hidden="true"></i>
-                </div>
-                <p>{marca}</p>
-                <p>{fabrication_time}</p>
-                <button className="btn btn-primary">
-                    <i className="fa fa-save mx-2" aria-hidden="true"></i>
-                </button>
+  const [showInput, setShowInput] = useState(false);
+  const { marca, fabrication_time } = props.car;
 
-            </div>
+  return (
+    <div className="p-3 col-md-3">
+      <div className="card justify-content-center p-1">
+        <div className="d-flex justify-content-around">
+          <i
+            className="fa fa-heart mx-2 btn btn-outline-danger"
+            aria-hidden="true"
+          ></i>
+          <i
+            className="fa fa-pencil mx-2 btn btn-warning"
+            aria-hidden="true"
+            onClick={() => setShowInput(!showInput)}
+          ></i>
+          <i className="fa fa-trash mx-2 btn btn-danger" aria-hidden="true"></i>
         </div>
-    )
-}
-export default Car
+        <p className="py-3">{marca}</p>
+        {showInput ? (
+          <input className="form-control text-center" value={marca} />
+        ) : (
+          ""
+        )}
+        <p>{fabrication_time}</p>
+        {showInput ? (
+          <input
+            className="form-control text-center"
+            value={fabrication_time}
+          />
+        ) : (
+          ""
+        )}
+        <button className="btn btn-primary mt-2">
+          <i className="fa fa-save mx-2" aria-hidden="true"></i>
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Car;
+
--- src/components/cars/cars.jsx
+++ src/components/cars/cars.jsx
@@ -1,34 +1,33 @@
 import React, { useEffect, useState } from "react";
-import Car from './car';
-import Sidebar from '../sidebar/sidebar'
+import Car from "./car";
+import Sidebar from "../sidebar/sidebar";
 
-const Cars = (props) => {
-    const [cars, setCars] = useState([]);
-    useEffect(() => {
-        getCars();
-    }, []);
+const Cars = () => {
+  const [cars, setCars] = useState([]);
+  useEffect(() => {
+    getCars();
+  }, []);
 
-    const getCars = async () => {
-        let url = "http://127.0.0.1:4000/car";
-        let response = await fetch(url);
-        let res = await response.json();
-        let carlist = res.cars
+  const getCars = async () => {
+    let url = "http://127.0.0.1:4000/car";
+    let response = await fetch(url);
+    let res = await response.json();
+    let carlist = res.cars;
 
-        setCars(() => carlist);
-    };
-    return (
-        <>
-            <div className="row m-2 p-3">
-                <div>
-                    <Sidebar />
-                </div>
-                {cars.map((car) =>
-                (
-                    <Car key={car._id} car={car} />
-                )
-                )}
-            </div>
-        </>
-    )
-}
-export default Cars
+    setCars(() => carlist);
+  };
+  return (
+    <>
+      <div className="row m-2 p-3">
+        <div>
+          <Sidebar />
+        </div>
+        {cars.map((car) => (
+          <Car key={car._id} car={car} />
+        ))}
+      </div>
+    </>
+  );
+};
+export default Cars;
+
